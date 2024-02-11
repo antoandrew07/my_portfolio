de54a44356d30bd842edb74c8367c29ad346391f
antoandrew07 <andrewsmicheal49@gmail.com>
2024-02-11 21:24:03 +0530

    commit done

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -4,13 +4,15 @@ import bg from '../../assests/profile.png'
 
 // import {Link} from 'react-scroll'
 const Intro = () => {
+  const URL = 'https://drive.google.com/file/d/1feLfzG8zIvFuLlOv5nzxv6AEpTOotcFh/view?usp=drive_link'
   const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = 'https://drive.google.com/uc?export=download&id=1LcswAoz8NHoyUd6SVMbo8FG6ZnBcQcpq'; // Replace 'path/to/your/file.pdf' with the actual path to your PDF file
-    link.download = 'ANDREWSMICHEAL_RESUME.pdf'; // Replace 'filename.pdf' with the desired name for the downloaded file
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    window.open(URL, '_blank');
+    // const link = document.createElement('a');
+    // link.href = URL ; // Replace 'path/to/your/file.pdf' with the actual path to your PDF file
+    // link.download = 'ANDREWSMICHEAL_RESUME.pdf'; // Replace 'filename.pdf' with the desired name for the downloaded file
+    // document.body.appendChild(link);
+    // link.click();
+    // document.body.removeChild(link);
   };
   return (
     <section id="intro">
