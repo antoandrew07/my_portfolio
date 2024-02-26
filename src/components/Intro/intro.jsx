eef61dfd8c9fa2c6a9078061aba7b3669d7211fb
antoandrew07 <andrewsmicheal49@gmail.com>
2024-02-27 00:36:15 +0530

    type writter

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './intro.css'
 import bg from '../../assests/profile.png'
-
+import Typewriter from 'typewriter-effect';
 // import {Link} from 'react-scroll'
 const Intro = () => {
   const URL = 'https://drive.google.com/file/d/1feLfzG8zIvFuLlOv5nzxv6AEpTOotcFh/view?usp=drive_link'
@@ -12,7 +12,7 @@ const Intro = () => {
     <section id="intro">
         <div className="introContent">
             <span className="hello">Hello,</span>
-            <span className="introText">I'm <span className='introName'>Andrews</span><br />Web Developer</span>
+            <span className="introText">I'm <span className='introName'>Andrews</span><br /><Typewriter options={{strings:["Web Developer","Front-end Developer"],autoStart:true,loop:true}}/></span>
             <p className="introPara">Crafting digital experiences through innovative web solutions,<br /> I specialize in creating responsive and visually stunning websites </p>
             <button className="btn" onClick={handleDownload} >My CV</button>
             
