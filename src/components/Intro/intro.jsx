536b8438fa5941053226bf6eb91630ea41e4eca5
antoandrew07 <andrewsmicheal49@gmail.com>
2024-02-27 01:31:29 +0530

    mobile

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -12,7 +12,7 @@ const Intro = () => {
     <section id="intro">
         <div className="introContent">
             <span className="hello">Hello,</span>
-            <span className="introText">I'm <span className='introName'>Andrews</span><br /><Typewriter options={{strings:["Web Developer","Front-end Developer"],autoStart:true,loop:true}}/></span>
+            <span className="introText">I'm <span className='introName'>Andrews</span><br /><Typewriter options={{strings:["Web Developer","Frontend Developer"],autoStart:true,loop:true}}/></span>
             <p className="introPara">Crafting digital experiences through innovative web solutions,<br /> I specialize in creating responsive and visually stunning websites </p>
             <button className="btn" onClick={handleDownload} >My CV</button>
             
