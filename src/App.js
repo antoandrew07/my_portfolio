48568fdb13f3a756e54c58237a889da961b6624b
antoandrew07 <andrewsmicheal49@gmail.com>
2024-02-26 23:00:37 +0530

    reveal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
+import { useEffect, useRef } from "react";
 import Certificate from "./components/Certificate/certificate";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
 import Intro from "./components/Intro/intro";
 import Navbar from "./components/NavBar/navbar";
 import Skills from "./components/skills/skills";
+import { motion, useInView, useScroll} from "framer-motion"
 
 function App() {
+  const {scrollYProgress} = useScroll();
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  useEffect(()=>{
+    //console.log(isInView);
+  },[isInView])
+  const style = {
+    opacity: isInView? 1:0,
+    transition: "5s opacity"
+  }
+  // const scrollX = useSpring(scrollYProgress)
+
   return (
     <div className="App">
+      
       <Navbar />
+      <motion.div style={{
+        scaleX:scrollYProgress,
+        background:'#FF6F00',
+        transformOrigin:'left',
+        position : 'sticky',
+        top : 80,
+        width : "100%",
+        height:"2px",
+        zIndex:1000
+       }}/>
       <Intro />
-      <Skills />
+      <div
+        ref={ref}
+        style={style}
+      >
+        <Skills />
+      </div>
       <Certificate />
       <Contact />
       <Footer />
--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './navbar.css'
-import logo from '../../assests/logos.png'
+
 
 import menu from '../../assests/mobMenu.png'
 import {Link} from 'react-scroll'
@@ -8,7 +8,8 @@ const Navbar = () => {
   const [showMenu , setShoeMenu] = useState(false);
   return (
     <nav className="navbar">
-        <img src={logo} alt="logo" className='logo'/>
+        {/* <img src={logo} alt="logo" className='logo'/> */}
+        <div className="logo"></div>
         <div className="desktopMenu">
             <Link activeClass="active" to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link> 
             <Link activeClass="active" to='skills' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">Skills</Link>
